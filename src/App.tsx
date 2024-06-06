55cdb89fa71599c825412eb92aa75fb95d9d5652
phuc_le_ngoc <lengocphuc0099@gmail.com>
2024-06-06 23:54:38 +0700

    add css3

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
+import { useEffect } from "react";
 import "./App.css";
 
 function App() {
+  // useEffect(() => {
+  //   function ensureDocumentIsScrollable() {
+  //     const isScrollable =
+  //       document.documentElement.scrollHeight > window.innerHeight;
+  //     if (!isScrollable) {
+  //       document.documentElement.style.setProperty(
+  //         "height",
+  //         "calc(100vh + 1px)",
+  //         "important"
+  //       );
+  //     }
+  //   }
+  //   function preventCollapse() {
+  //     if (window.scrollY === 0) {
+  //       window.scrollTo(0, 1);
+  //     }
+  //   }
+
+  //   const scrollableElement = document.querySelector(".scrollable-element");
+  //   scrollableElement?.addEventListener("touchstart", preventCollapse);
+
+  //   window.addEventListener("load", ensureDocumentIsScrollable);
+  // });
+
   return (
     <div className="scrollable-element">
-      <div>Item 1</div>
-      <div>Item 2</div>
-      <div>Item 3</div>
-      <div>Item 4</div>
-      <div>Item 5</div>
-      <div>Item 6</div>
-      <div>Item 7</div>
-      <div>Item 8</div>
-      <div>Item 9</div>
-      <div>Item 10</div>
-    </div>
+    <div>Item 1</div>
+    <div>Item 2</div>
+    <div>Item 3</div>
+    <div>Item 4</div>
+    <div>Item 5</div>
+    <div>Item 6</div>
+    <div>Item 7</div>
+    <div>Item 8</div>
+    <div>Item 9</div>
+    <div>Item 10</div>
+  </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from "./App";
 import "./index.css";
 // this manifest is used temporarily for development purposes
 
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
 );

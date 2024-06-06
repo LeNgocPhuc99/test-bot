7ba1ebb1d7a9b71321dbf1f67899d979eee733e1
phuc_le_ngoc <lengocphuc0099@gmail.com>
2024-06-07 00:01:53 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -27,18 +27,23 @@ function App() {
   // });
 
   return (
-    <div className="scrollable-element">
-    <div>Item 1</div>
-    <div>Item 2</div>
-    <div>Item 3</div>
-    <div>Item 4</div>
-    <div>Item 5</div>
-    <div>Item 6</div>
-    <div>Item 7</div>
-    <div>Item 8</div>
-    <div>Item 9</div>
-    <div>Item 10</div>
-  </div>
+    <div>
+      <div
+        style={{ width: "100%", height: "200px", backgroundColor: "red" }}
+      ></div>
+      <div className="scrollable-element">
+        <div>Item 1</div>
+        <div>Item 2</div>
+        <div>Item 3</div>
+        <div>Item 4</div>
+        <div>Item 5</div>
+        <div>Item 6</div>
+        <div>Item 7</div>
+        <div>Item 8</div>
+        <div>Item 9</div>
+        <div>Item 10</div>
+      </div>
+    </div>
   );
 }
 

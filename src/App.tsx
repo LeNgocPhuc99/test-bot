e73a2a786882b2400362d6d1751c1671550a6f7c
phuc_le_ngoc <lengocphuc0099@gmail.com>
2024-06-07 10:45:20 +0700

    remove useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -4,73 +4,73 @@ import "./App.css";
 const tg = Telegram.WebApp;
 
 function App() {
-  useEffect(() => {
-    if (!tg) return;
-    if (window.tgAppInited) return;
-    window.tgAppInited = true;
-    const scrollableEl = document.getElementById("scrollable-el");
-    // ???
-    console.log("Init Tg");
-    tg.expand();
-    const overflow = 100;
-    function setupDocument(enable: boolean) {
-      if (enable) {
-        document.body.style.marginTop = `${overflow}px`;
-        document.body.style.height = window.innerHeight + overflow + "px";
-        document.body.style.paddingBottom = `${overflow}px`;
-        window.scrollTo(0, overflow);
-      } else {
-        document.body.style.removeProperty("marginTop");
-        document.body.style.removeProperty("height");
-        document.body.style.removeProperty("paddingBottom");
-        window.scrollTo(0, 0);
-      }
-    }
+  // useEffect(() => {
+  //   if (!tg) return;
+  //   if (window.tgAppInited) return;
+  //   window.tgAppInited = true;
+  //   const scrollableEl = document.getElementById("scrollable-el");
+  //   // ???
+  //   console.log("Init Tg");
+  //   tg.expand();
+  //   const overflow = 100;
+  //   function setupDocument(enable: boolean) {
+  //     if (enable) {
+  //       document.body.style.marginTop = `${overflow}px`;
+  //       document.body.style.height = window.innerHeight + overflow + "px";
+  //       document.body.style.paddingBottom = `${overflow}px`;
+  //       window.scrollTo(0, overflow);
+  //     } else {
+  //       document.body.style.removeProperty("marginTop");
+  //       document.body.style.removeProperty("height");
+  //       document.body.style.removeProperty("paddingBottom");
+  //       window.scrollTo(0, 0);
+  //     }
+  //   }
 
-    setupDocument(true);
+  //   setupDocument(true);
 
-    let ts: number | undefined;
-    const onTouchStart = (e: TouchEvent) => {
-      ts = e.touches[0].clientY;
-    };
-    const onTouchMove = (e: TouchEvent) => {
-      if (scrollableEl) {
-        console.warn("onTouchMove");
-        const scroll = scrollableEl.scrollTop;
-        const te = e.changedTouches[0].clientY;
-        if (scroll <= 0 && ts! < te) {
-          e.preventDefault();
-        }
-      } else {
-        e.preventDefault();
-      }
-    };
-    document.documentElement.addEventListener("touchstart", onTouchStart, {
-      passive: false,
-    });
-    document.documentElement.addEventListener("touchmove", onTouchMove, {
-      passive: false,
-    });
+  //   let ts: number | undefined;
+  //   const onTouchStart = (e: TouchEvent) => {
+  //     ts = e.touches[0].clientY;
+  //   };
+  //   const onTouchMove = (e: TouchEvent) => {
+  //     if (scrollableEl) {
+  //       console.warn("onTouchMove");
+  //       const scroll = scrollableEl.scrollTop;
+  //       const te = e.changedTouches[0].clientY;
+  //       if (scroll <= 0 && ts! < te) {
+  //         e.preventDefault();
+  //       }
+  //     } else {
+  //       e.preventDefault();
+  //     }
+  //   };
+  //   document.documentElement.addEventListener("touchstart", onTouchStart, {
+  //     passive: false,
+  //   });
+  //   document.documentElement.addEventListener("touchmove", onTouchMove, {
+  //     passive: false,
+  //   });
 
-    const onScroll = () => {
-      if (window.scrollY < overflow) {
-        window.scrollTo(0, overflow);
-        if (scrollableEl) {
-          scrollableEl.scrollTo(0, 0);
-        }
-      }
-    };
-    window.addEventListener("scroll", onScroll, { passive: true });
+  //   const onScroll = () => {
+  //     if (window.scrollY < overflow) {
+  //       window.scrollTo(0, overflow);
+  //       if (scrollableEl) {
+  //         scrollableEl.scrollTo(0, 0);
+  //       }
+  //     }
+  //   };
+  //   window.addEventListener("scroll", onScroll, { passive: true });
 
-    // authorize here
+  //   // authorize here
 
-    return () => {
-      setupDocument(false);
-      document.documentElement.removeEventListener("touchstart", onTouchStart);
-      document.documentElement.removeEventListener("touchmove", onTouchMove);
-      window.removeEventListener("scroll", onScroll);
-    };
-  }, [tg]);
+  //   return () => {
+  //     setupDocument(false);
+  //     document.documentElement.removeEventListener("touchstart", onTouchStart);
+  //     document.documentElement.removeEventListener("touchmove", onTouchMove);
+  //     window.removeEventListener("scroll", onScroll);
+  //   };
+  // }, [tg]);
 
   return (
     <div>

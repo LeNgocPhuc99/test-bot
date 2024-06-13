eb96a5c0f77570205d8814589eb756bffdabc048
phuc_le_ngoc <lengocphuc0099@gmail.com>
2024-06-13 21:04:42 +0700

    test

--- src/core/views/quest/TelegramRefReward.tsx
+++ src/core/views/quest/TelegramRefReward.tsx
@@ -104,7 +104,7 @@ const TelegramRefReward = ({ handleMoveToRank }: TelegramRefRewardProps) => {
               fontSize: { xs: "0.938rem", sm: "1.375rem", md: "1.875rem" },
             }}
           >
-            {t("TelegramRefReward|title")}
+            {t("TelegramRefReward|title")} test
           </Typography>
         </Box>
         <ReloadDataButton reloadFunction={handleRefreshData} />
